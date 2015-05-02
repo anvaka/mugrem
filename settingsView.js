9bb0cc45c18c45f98c2699880c9116566cb0f33b
anvaka <anvaka@gmail.com>
2015-05-02 13:49:29 -0700

    Allow min/max color change

--- settingsView.js
+++ settingsView.js
@@ -6,12 +6,22 @@ function createSettingsView(layout, renderer, dim) {
   var gui = new exdat.GUI();
   createDimensionMapping(gui, layout, dim);
   var folder = gui.addFolder('View settings');
-  var model = { color: 0x8e8e8e };
+  var model = {
+    color: 0x8e8e8e,
+    startColor: 0x000000,
+    endColor: 0xffffff
+  };
   folder.addColor(model, 'color').name('Background color').onChange(updateClearColor);
+  folder.addColor(model, 'startColor').name('Fartherst color').onChange(updateNodeColor);
+  folder.addColor(model, 'endColor').name('Nearest color').onChange(updateNodeColor);
   folder.open();
 
   function updateClearColor(color) {
     renderer.clearColor(color);
+  }
+
+  function updateNodeColor() {
+    layout.setColorRange(model.startColor, model.endColor);
   }
 }
 
